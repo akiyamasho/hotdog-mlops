1a2aafb83798485984fde857c5f9c227347b178f
ショウ <35907066+akiyamasho@users.noreply.github.com>
2021-08-25 23:41:47 +0900

    MAINT: added loading of model in simple backend

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,31 @@
 # Not hotdog API
 
-`gunicorn -b :5000 -t 10 --chdir src --reload wsgi:app`
+### Setup
+
+##### Model
+
+1. Package a trained model in in `hotdog.ipynb` in `../research` using `model.save("model")`
+
+2. Copy the `model` folder to the root of this backend project. The `model` folder should have the following structure:
+
+├── model
+│   ├── assets
+│   ├── variables
+│   ├── keras_metadata.pb
+│   ├── saved_model.pb
+
+### Running
+
+1. Run the Flask app with `gunicorn`
+
+```
+gunicorn -b :5000 -t 60 --chdir src --reload wsgi:app
+```
+
+2. Send a POST request with a `.jpg` file to the API to get predictions
+
+```
+curl -X POST -F image_file=@test.jpg http://localhost:5000
+```
+
+You should be getting a prediction 
--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,44 @@
-from flask import Flask
+import numpy as np
+
+from tensorflow import keras
+from PIL import Image
+from keras.applications.vgg16 import preprocess_input
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+IMAGE_SIZE_PX = 200
+MODEL_PATH = "../model"
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+model = keras.models.load_model(MODEL_PATH)
+
+
+def convert_image_to_tensor(image):
+    img = Image.open(image)
+    img = img.convert("RGB")
+    img = img.resize((IMAGE_SIZE_PX, IMAGE_SIZE_PX))
+
+    img = np.array(img)
+
+    img = preprocess_input(img)
+
+    return img
+
+
+def predict(img_tensors):
+    probs = model.predict(np.expand_dims(img_tensors, axis=0))
+    pred = probs.argsort()[0][::-1][0]
+
+    if pred == 1.0:
+        return "hotdog"
+    else:
+        return "not hotdog"
+
+
+@app.route("/", methods=["POST"])
+def classify_hotdog():
+    image = request.files.get("image_file", None)
+    img_tensors = convert_image_to_tensor(image=image)
+    prediction = predict(img_tensors=img_tensors)
+
+    return jsonify(prediction=prediction)
